3ef632cb79b4e90420bb8f2624c4b62a471f60e6
Sproff <sproff.oluwaseun@gmail.com>
2021-05-07 11:43:19 +0100

    testing

--- src/components/common/SearchInput.jsx
+++ src/components/common/SearchInput.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+    import React, { useEffect, useState } from "react"
 import {
   Box,
   Stack,
@@ -7,11 +7,37 @@ import {
   Input, 
 } from "@chakra-ui/react"
 import { BsSearch } from "react-icons/bs";
+import instance from "../../queries/axios.config";
+
 
 export const SearchInput = () => {
+  const [search, setSearch] = useState("")
+  const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const getAllProducts = async () => {
+			try {
+				const { data } = await instance.get("/products");
+        const result = data.data.products;
+        console.log(result);
+        setProducts(result)
+        setSearch(result)
+			} catch (err) {
+				console.log(err);
+			}
+
+      const filteredResult = products.filter((product) => {
+        return product.name.toLowerCase().includes(search);
+      });
+      // console.log(filteredResult);
+      setSearch(filteredResult)
+    };
+    getAllProducts();
+	}, []);
+
   return (
     <Stack spacing={4}>
-      <InputGroup bg="#fafafa" borderRadius="10px">
+      <InputGroup bg="#fafafa" borderRadius="10px" mt="4rem">
         <InputLeftElement
           pointerEvents="none"
         >
@@ -20,6 +46,10 @@ export const SearchInput = () => {
           </Box>
         </InputLeftElement>
         <Input
+          onChange={(e) => {
+            setSearch(e.target.value.toLowerCase())
+            console.log("clicked")
+          }}
           type="search"
           placeholder="Search product"
           border="none"
