7ee1db97339c3c198d98e50ccc1d43dba3e53a3b
Sproff <sproff.oluwaseun@gmail.com>
2021-10-16 17:39:10 +0100

    fix: 🐛 fixed the blank banner section

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,31 +1,33 @@
 import React from "react";
-import { Box, Image} from "@chakra-ui/react";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import { Box, Image, Stack, Text} from "@chakra-ui/react";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import Slider from "react-slick";
 
-import BannerImage1 from "../../assets/images/slide1.jpg";
-import BannerImage2 from "../../assets/images/slide2.jpg";
-import BannerImage3 from "../../assets/images/slide3.jpg";
+// import BannerImage1 from "../../assets/images/slide1.jpg";
+// import BannerImage2 from "../../assets/images/slide2.jpg";
+// import BannerImage3 from "../../assets/images/slide3.jpg";
+// import BannerImage3 from "../../assets/images/slide3.jpg";
+import BannerImage from "../../assets/images/pr1.jpg";
 
 export const Banner = () => {
-  const settings = {
-    dots: true,
-		fade: true,
-		speed: 500,
-		slidesToShow: 1,
-    slidesToScroll: 1,
-    // autoplay: true,
-	};
+  // const settings = {
+  //   dots: true,
+	// 	fade: true,
+	// 	speed: 500,
+	// 	slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   // autoplay: true,
+	// };
 	return (
 		<Box
-			// bg="linear-gradient(to right, #000, #968b8b)"
+			bg="linear-gradient(to right, #000, #968b8b)"
 			mt="1rem"
 			h="100%"
 			w="100%"
 			borderRadius="10px"
 		>
-			<Slider {...settings}>
+			{/* <Slider {...settings}>
 				<Box h="600px" w="300px">
           <Image src={BannerImage1} objectFit="cover" alt="banner-image-one"/>
 				</Box>
@@ -35,8 +37,8 @@ export const Banner = () => {
 				<Box  h="600px">
         <Image src={BannerImage3} objectFit="cover" alt="banner-image-three"/>
 				</Box>
-			</Slider>
-				{/* <Stack direction={["column", "column", "row", "row"]}>
+			</Slider> */}
+				<Stack direction={["column", "column", "row", "row"]}>
 					<Box
 						w="100%"
 						// h="auto"
@@ -62,7 +64,7 @@ export const Banner = () => {
 							backgroundPosition="center"
 						/>
 					</Box>
-				</Stack> */}
+				</Stack>
 		</Box>
 	);
 };
